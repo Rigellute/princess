6295461db1225df5aca3c9ecd86134ff6023bb7d
Alexander Keliris <rigellute@gmail.com>
2018-05-13 10:37:15 +0100

    Add cors

--- backend/mix.exs
+++ backend/mix.exs
@@ -33,14 +33,15 @@ defmodule Backend.Mixfile do
   # Type `mix help deps` for examples and options.
   defp deps do
     [
-      {:phoenix, "~> 1.3.2"},
-      {:phoenix_pubsub, "~> 1.0"},
+      {:cors_plug, "~> 1.5"},
+      {:cowboy, "~> 1.0"},
+      {:gettext, "~> 0.11"},
       {:phoenix_ecto, "~> 3.2"},
-      {:postgrex, ">= 0.0.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:phoenix_pubsub, "~> 1.0"},
+      {:phoenix, "~> 1.3.2"},
+      {:postgrex, ">= 0.0.0"},
     ]
   end
 
